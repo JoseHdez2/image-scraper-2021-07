692454208a559a30c4ef2a26710802f0ea0f3c3b
Jose Hernandez <josemanuelhdez2@gmail.com>
2021-07-04 13:36:20 +0100

    setup to serve the static frontend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImagesModule } from './images/images.module';
 
 @Module({
-  imports: [ImagesModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/dist'),
+    }),
+    ImagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
