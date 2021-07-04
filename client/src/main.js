a0af51e0b29e2c67830581bfab18c58e1a30416a
Jose Hernandez <josemanuelhdez2@gmail.com>
2021-07-04 22:54:04 +0100

    add pagination component to client.

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import JwPagination from 'jw-vue-pagination';
+Vue.component('jw-pagination', JwPagination);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
