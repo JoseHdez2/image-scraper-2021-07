aafdc1ed72b3747fdd71dbd3abc76e5d28d8313c
Jose Hernandez <josemanuelhdez2@gmail.com>
2021-07-04 13:11:19 +0100

    nest g resource images

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ImagesModule } from './images/images.module';
 
 @Module({
-  imports: [],
+  imports: [ImagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
