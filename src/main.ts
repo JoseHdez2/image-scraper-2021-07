13fc4d6a38b5aba889aa1a28848a9705c276df87
Jose Hernandez <josemanuelhdez2@gmail.com>
2021-07-04 13:11:07 +0100

    CORS and Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Image Scraping API')
+    .setDescription('API for scraping and storing images from websites.')
+    .setVersion('0.1')
+    .build();
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/', app, swaggerDoc);
+
   await app.listen(3000);
 }
 bootstrap();
