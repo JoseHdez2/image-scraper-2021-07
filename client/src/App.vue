3555b3077b59078b27ca4ecb22e06e236ef6b53b
Jose Hernandez <josemanuelhdez2@gmail.com>
2021-07-04 19:17:39 +0100

    client ImageList wip

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ImageList />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ImageList from './components/ImageList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ImageList
   }
 }
 </script>
---
+++ client/src/components/ImageList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card text-center m-3">
+        Example result: {{ exampleResult }}
+        <h3 class="card-header">Vue.js Pagination Tutorial & Example</h3>
+        <div class="card-body">
+            <div v-for="item in pageOfItems" :key="item.id">{{item.name}}</div>
+        </div>
+        <div class="card-footer pb-0 pt-3">
+            <jw-pagination :items="exampleItems" @changePage="onChangePage"></jw-pagination>
+        </div>
+    </div>
+</template>
+
+<script>
+// an example array of items to be paged
+const exampleItems = [...Array(150).keys()].map(i => ({ id: (i+1), name: 'Item ' + (i+1) }));
+let exampleResult;
+fetch('localhost:3000/api/images').then(
+  data => { exampleResult = data; console.log(exampleResult) }, 
+  error => { console.error(error); }
+);
+
+export default {
+    name: 'ImageList',
+    data() {
+        return {
+           exampleItems,
+            pageOfItems: [],
+            exampleResult
+        };
+    },
+    methods: {
+        onChangePage(pageOfItems) {
+            // update page of items
+            this.pageOfItems = pageOfItems;
+        }
+    }
+};
+</script>
